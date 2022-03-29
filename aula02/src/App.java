7a1c0133146edf844c8ef61d2f6e8632dcfc4b92
raphael <raphael@callist.com.br>
2022-03-29 14:42:12 -0300

    add aula02

--- aula02/src/App.java
+++ aula02/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World POO with gettes and setters!");
+        float tamanhoTela;
+        String nome;
+        celular.Celular celularA = new celular.Celular();
+        celularA.setNome("Xperia2");
+        celularA.setTamanhoTela(7.2f);
+        // as linhas 5 e 6 preenchem os dados do celularA através de métodos set
+
+        nome = celularA.getNome();
+        tamanhoTela = celularA.getTamanhoTela();
+        //as linhas 11 e 12 usam os métodos para obtermos os dados informados e podermos usar em variáveis 
+
+        celularA.mostrarPropriedadesViaParam(nome, tamanhoTela);
+        // a linha 15 mostra o uso de um método passando parametros (nome e tamanhoTela)
     }
 }
---
+++ aula02/src/celular/Celular.java
@@ -0,0 +1,58 @@
+package celular;
+public class Celular {
+    // ## variáveis nas linhas 4 e 5
+    private String nome;
+    private float tamanhoTela;
+    
+    // se um atributo é private, você não consegue usar em outros arquivos, 
+    //para acessá-los você cria métodos public como os getters e setters
+    
+    // ## métodos getters e setters
+
+    public String getNome(){
+        // se não for void precisa de return
+        return nome;
+    }
+
+    public float getTamanhoTela(){
+        // se não for void precisa de return
+        return tamanhoTela;
+    }
+
+    public void setNome(String nome){
+        // void não precisa de return
+        // this é usado para poder repetir o nome do parâmetro aqui em baixo
+        // this faz referencia ao atributo ou método interno
+        this.nome = nome;
+    }
+
+    public void setTamanhoTela(float tamanhoTela){
+        // void não precisa de return
+        // this é usado para poder repetir o nome do parâmetro aqui em baixo
+        // this faz referencia ao atributo ou método interno
+        this.tamanhoTela = tamanhoTela;
+    }
+    
+    
+    // ## demais métodos nas linhas abaixo:
+    
+    public void atualizarPropriedades(String VNome, float vTamanhoTela)
+    {
+        nome = VNome;
+        tamanhoTela = vTamanhoTela;
+        //se for usar o this pode deixar os parâmetros com o mesmo nome dos atributos
+        // após isso usar o this como no método setNome
+    }
+
+    public void mostrarPropriedades()
+    {
+        System.out.println(nome + '\n' + tamanhoTela);
+    }
+
+    public void mostrarPropriedadesViaParam(String nome, float tamanhoTela)
+    {
+        System.out.println(nome + '\n' + tamanhoTela);
+    }
+
+};
+
